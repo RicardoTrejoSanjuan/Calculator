68513967c9a65830375490c109535f641ac6e41b
RicardoTrejoSanjuan <trejosanjuanricardo@gmail.com>
2018-10-14 14:36:03 -0500

    Calculadora Memory

--- app/calculadora/calculadora.js
+++ app/calculadora/calculadora.js
@@ -1,30 +1,106 @@
-function calCtrl(operaciones){
-    this.$onInit = function(){
+'use strict';
 
-        this.answer = null;
-        this.value1 = 0;
-        this.value2 = 0;
-        this.operador = '';
+function calCtrl(operator, memory) {
 
-        this.setOperador = function (operador){
-            this.operador = angular.copy(operador)
-            debugger;
-            if (!this.answer.isNaN()){
-                this.value1 = angular.copy(this.answer)
-            } else {
-                alert("Se requiere un numero valido!!");
-            }
+    var vm = this;
 
-        };
+    vm.answer = null;
+    vm.operator = null;
+    vm.value = null;
 
-        // operaciones.suma();
+    vm.$onInit = function () {
+        vm.answer = '';
+        vm.value = NaN;
+        vm.operator = '';
+        localStorage.setItem("memoryCal", 0);
     }
 
-    
+    // resolver operacion
+    vm.resolve = function () {
+        if (vm.answer != "") {
+            var result = operator.resolveOperation(vm.answer);
+            if (result == "Infinity") {
+                alert("Infinity");
+                return 0
+            } else if (result != null && result != "Infinity") {
+                return result
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    vm.equals = function () {
+        var result = vm.resolve();
+        if (result != false) {
+            vm.answer = result;
+        } else {
+            vm.answer = '';
+        }
+    }
+
+    // Limpiar memoria
+    this.memoryClear = function () {
+        memory.memoryClear();
+        vm.answer = '';
+    }
+
+    // Suma el número mostrado a otro número que se encuentre en memoria
+    vm.memoryAdd = function () {
+        var result = vm.resolve();
+        if (result != false) {
+            memory.memoryAdd(result);
+        }
+        vm.answer = ''
+    }
+
+    // Resta el número mostrado a otro número que se encuentre en memoria
+    vm.memorySubtractions = function () {
+        var result = vm.resolve();
+        if (result != false) {
+            memory.memorySubtractions(result);
+        }
+        vm.answer = '';
+    }
+
+    // Almacenar en memoria
+    vm.memoryStorage = function () {
+        var result = vm.resolve();
+        if (result != false) {
+            memory.memoryStorage(result);
+        }
+        vm.answer = '';
+    }
+
+    // Recuperar memoria    
+    vm.memoryRecall = function () {
+        vm.answer = memory.memoryRecall();
+    }
+
+    // Elimina el número mostrado
+    vm.clearError = function () {
+        vm.answer = '';
+    }
+
+    // Elimina todo el cálculo actual. 
+    vm.clearFunction = function () {
+        vm.answer = '';
+    }
+
+    // Eliminar
+    vm.delete = function () {
+        vm.answer = vm.answer.substring(0, vm.answer.length - 1);
+    }
+
+    vm.parseValue = function (num) {
+        return parseFloat(num);
+    }
 }
 
 angular.module("app").component("calculadora", {
-    bindings:{},
+    bindings: {},
     templateUrl: "app/calculadora/calculadora.html",
     controller: calCtrl
 });
